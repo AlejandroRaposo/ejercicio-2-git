158322b4c6ae6a8ad3c2d054433ac41e93a830d8
daweb048 <daweb048@ejemplo.com>
2023-11-13 10:52:20 +0100

    Creado operaciones.py con sumar restar y multiplicar

---
+++ operaciones.py
@@ -0,0 +1,28 @@
+def sumar():
+    numero1= input("Introduce el primer número:")
+    numero2 = input("Introduce el segundo número")
+    if ((isinstance(numero1, int) or (isinstance(numero1, float)))
+     and (isinstance(numero2, int) or (isinstance(numero2, float)))):
+     return numero1+numero2
+
+
+
+
+
+def restar():
+     numero1= input("Introduce el primer número:")
+    numero2 = input("Introduce el segundo número")
+    if ((isinstance(numero1, int) or (isinstance(numero1, float)))
+     and (isinstance(numero2, int) or (isinstance(numero2, float)))):
+     return numero1-numero2
+
+
+def multiplicar():
+     numero1= input("Introduce el primer número:")
+    numero2 = input("Introduce el segundo número")
+    if ((isinstance(numero1, int) or (isinstance(numero1, float)))
+     and (isinstance(numero2, int) or (isinstance(numero2, float)))):
+     return numero1*numero2
+
+
+
